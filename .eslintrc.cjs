1adf8fabce4795795bf202d28740e8f4440b0af3
LuticaCANARD <presan100@gmail.com>
2024-01-03 22:07:44 +0900

    adding AirBnB style

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,14 +5,31 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'airbnb'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh','@typescript-eslint'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
-  },
+    "react/jsx-filename-extension": [1, { "allow": "as-needed", "extensions": [".js", ".jsx", ".tsx", ".ts"] }],
+    'linebreak-style': "off",
+  }, 'jsx-runtime': {
+    plugins: [
+      'react'
+    ],
+    parserOptions: {
+      ecmaFeatures: {
+        jsx: true
+      },
+      jsxPragma: null // for @typescript/eslint-parser
+    },
+    rules: {
+      'react/react-in-jsx-scope': 0,
+      'react/jsx-uses-react': 0
+    }
+  }
 }
