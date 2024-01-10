bfb7a8bf354f90ecdf3b8d8175235eae947527d5
okxooxoo <okxooxoo@naver.com>
2024-01-10 19:31:29 +0900

    ✨ feat: 네이버 지도 API 연동 및 주소 검색 기능 구현

---
+++ src/Map.tsx
@@ -0,0 +1,28 @@
+import { useEffect } from "react";
+import "./Map.css";
+
+function Map() {
+  const { naver } = window;
+
+  const drawMap = () => {
+    const map = new naver.maps.Map("map", {
+      center: new naver.maps.LatLng(37.3595316, 127.1052133),
+      zoom: 15,
+      mapTypeControl: true,
+    });
+  };
+
+  useEffect(() => {
+    drawMap();
+  }, []);
+
+  return (
+    <>
+      <body>
+        <div id="map"></div>
+      </body>
+    </>
+  );
+}
+
+export default Map;
