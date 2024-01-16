369e34120dc1de1ba75745d5c9d99215e27ccb1e
Lutica_CANARD <presan100@gmail.com>
2024-01-16 11:00:55 +0900

    refactor : import

--- src/app/api/getRecomendation/route.ts
+++ src/app/api/getRecomendation/route.ts
@@ -2,6 +2,7 @@
 
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { NextResponse } from 'next/server';
+
 export async function GET(
   req: NextApiRequest,
   res: NextApiResponse,
