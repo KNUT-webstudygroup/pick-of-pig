ae39f0e13303928083fcacef4f297851eaa23997
Lutica_CANARD <presan100@gmail.com>
2024-01-19 00:05:48 +0900

    CI : nodejs_compat

--- src/app/api/getRecomendation/route.ts
+++ src/app/api/getRecomendation/route.ts
@@ -6,5 +6,5 @@ export async function GET(req: NextRequest) {
   console.log(req.nextUrl.searchParams.get('aa'));
   //const v = await fetch('http://localhost:3000');
   //-> next단에서 외부교신은 문제없는듯.
-  return NextResponse.json({ aa: 'kk' });
+  return NextResponse.json({ aa: 'kk' }); 
 }
