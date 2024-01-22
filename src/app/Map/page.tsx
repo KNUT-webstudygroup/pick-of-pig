af3651a78f759c3f7292d54ed925bf69e6dffde1
Lutica_CANARD <presan100@gmail.com>
2024-01-23 00:06:34 +0900

    ♻️ Refactor : 동작과 할당 분리

--- src/app/Map/page.tsx
+++ src/app/Map/page.tsx
@@ -14,14 +14,20 @@ function Map() {
   const handleSearchClick = () => {
     searchAddressToCoordinate(searchAddress);
   };
-
+  // 아마 차후에 추상팩토리같은 디자인 패턴을 적용해야 하지않을까 싶네요.
+  // 동작과 변수를 구분하는 영역인 것 같아서 나눴어요!
+  const addGoogleMap = (center : google.maps.LatLngLiteral) : google.maps.Map => (
+    new google.maps.Map(document.getElementById('map') as HTMLElement, {
+      center,
+      zoom: 15,
+    })
+  );
   const style = { width: '100vw', height: '100vh' };
   useEffect(() => {
     const center: google.maps.LatLngLiteral = { lat: 37.3595316, lng: 127.1052133 };
-    const map = new google.maps.Map(document.getElementById('map') as HTMLElement, {
-      center,
-      zoom: 15,
-    });
+    // 사용할 일이 없어서 객체로 할당 안해도 될것 같네요
+    // const map =
+    addGoogleMap(center);
   }, []);
 
   return (
