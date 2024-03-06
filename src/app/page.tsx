0e60cb56232517b3e7e3b2112a85fe9a72a9f51a
LuticaCANARD <presan100@gmail.com>
2024-03-06 12:46:36 +0900

    fix : delete not needed script

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,12 +6,6 @@ import GlobalStyle from '@/ui/GlobalStyle';
 import { RecoilRoot } from 'recoil';
 
 function Home() {
-  console.log(score); // undeFined
-
-  score = 80; // 값의 할당
-  let score; // 변수선언
-
-  console.log(score); // ??
   return (
     <>
       <MainPage />
